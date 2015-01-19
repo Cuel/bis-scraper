500f58ec93da41970fe31328ddcaa6f60efa6a3c
Cuel <alleee@gmail.com>
2015-01-19 01:23:16 +0100

    Start of command scrape

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-var scraper = require('./scraper');
-
-scraper.scrapeScriptingCommands(function scrapingDone(err, data) {
-    if (err) {
-        console.log(err);
-    }
-   //console.log(data);
-});
+module.exports = require('./lib/app.js');
